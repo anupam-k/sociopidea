a5ef1795594474c744a80b7d8ce98c1601cb8a03
anupam-k <anupamkrishnan4@gmail.com>
2023-02-14 15:43:23 +0530

    Database configured

--- server/index.js
+++ server/index.js
@@ -11,12 +11,12 @@ import { fileURLToPath } from "url";
 
 /*CONFIGURATIONS*/
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filenam);
+const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginEmbedderPolicy({ policy: "cross-origin" }));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -33,3 +33,15 @@ const storage = multer.diskStorage({
   },
 });
 const upload = multer({ storage });
+
+/*MONGOOSE SETUP*/
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
